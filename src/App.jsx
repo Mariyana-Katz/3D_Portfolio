b95f9cc9617bc2daa80e5fba85ebf49cdee8bc5e
Mariqna <mariqnak21@gmail.com>
2025-10-29 11:46:02 -0400

    adding the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
+      {/* z-0 is for layering */}
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
         <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
@@ -7,9 +7,11 @@ import { logo3, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 bg-secondary`}
+      className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 bg-black pl-0`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -21,18 +23,56 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img
-            src={logo3}
-            alt="logo"
-            style={{ width: "80px", height: "80px" }}
-            className=" pt-1 object-contain flex-none pr-2"
-          />
+          <img src={logo3} alt="logo" className="w-14 h-14 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
             Mariyana
             <span className="sm:block hidden">| Mariyana Katzarova</span>
           </p>
         </Link>
-        <p className="text-red-500" asdsa></p>
+        {/* navigation links */}
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[24px] h-[24px] object-contain cursor-pointer "
+            //this will toggle the menu
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-16 right-0 mx-4 my-2 min-w-[140px] rounded-xl`}
+          >
+            {" "}
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  //this is for closing the menu when a link is clicked
+                  onClick={() => { setToggle(!toggle); setActive(link.title); }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
