5682cbdd46c3ce6987df0a899594294ae51e5769
Mariqna <mariqnak21@gmail.com>
2025-11-20 17:00:58 -0500

    made some small changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I am <span className="text-[#915eff]"> Mariyana</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2`}>
+          <p className={`${styles.heroSubText} mt-1`}>
             {" "}
             Full-stack Software Developer with a background in customer-focused
             roles. Skilled in Java, React, and Spring Boot. Eager to contribute
@@ -29,9 +29,10 @@ const Hero = () => {
       <ComputersCanvas />
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          {/* className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2"}*/}
+          <div>
             {/* Animate the small circle to move up and down */}
-            <motion.div
+            {/* <motion.div
               animate={{
                 y: [0, 24, 0],
               }}
@@ -42,7 +43,7 @@ const Hero = () => {
                 repeatType: "loop",
               }}
               className="w-3 h-3 rounded-full bg-secondary mb-1"
-            />
+            /> */}
           </div>
         </a>
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
+import { j } from "maath/dist/misc-19a3ec46.esm";
 import {
   mobile,
   backend,
@@ -42,12 +43,12 @@ const services = [
     icon: web,
   },
   {
-    title: "React Developer",
-    icon: reactjs,
+    title: "Backend Developer",
+    icon: java,
   },
   {
-    title: "Backend Developer",
-    icon: backend,
+    title: "React Developer",
+    icon: reactjs,
   },
   {
     title: "Content Creator",
@@ -128,20 +129,11 @@ const experiences = [
 
 const testimonials = [
   {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    testimonial: "",
+    name: "",
+    designation: "",
+    company: "",
+    image: "",
   },
 ];
 
